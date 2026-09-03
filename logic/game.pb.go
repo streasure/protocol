d8c33b898783da8f216a76d35564330de0acdd7d
hejiayu <2501549691@qq.com>
2026-09-03 15:42:55 +0800

    删除冗余协议

--- logic/game.pb.go
+++ logic/game.pb.go
@@ -424,6 +424,108 @@ func (x *BanInfo) GetBanReason() string {
 	return ""
 }
 
+type HeartbeatReq struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	ClientTime int64 `protobuf:"varint,1,opt,name=client_time,json=clientTime,proto3" json:"client_time,omitempty"`
+}
+
+func (x *HeartbeatReq) Reset() {
+	*x = HeartbeatReq{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_logic_game_proto_msgTypes[5]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *HeartbeatReq) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*HeartbeatReq) ProtoMessage() {}
+
+func (x *HeartbeatReq) ProtoReflect() protoreflect.Message {
+	mi := &file_logic_game_proto_msgTypes[5]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use HeartbeatReq.ProtoReflect.Descriptor instead.
+func (*HeartbeatReq) Descriptor() ([]byte, []int) {
+	return file_logic_game_proto_rawDescGZIP(), []int{5}
+}
+
+func (x *HeartbeatReq) GetClientTime() int64 {
+	if x != nil {
+		return x.ClientTime
+	}
+	return 0
+}
+
+type HeartbeatAck struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	ServerTime int64 `protobuf:"varint,1,opt,name=server_time,json=serverTime,proto3" json:"server_time,omitempty"`
+	RttMs      int64 `protobuf:"varint,2,opt,name=rtt_ms,json=rttMs,proto3" json:"rtt_ms,omitempty"`
+}
+
+func (x *HeartbeatAck) Reset() {
+	*x = HeartbeatAck{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_logic_game_proto_msgTypes[6]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *HeartbeatAck) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*HeartbeatAck) ProtoMessage() {}
+
+func (x *HeartbeatAck) ProtoReflect() protoreflect.Message {
+	mi := &file_logic_game_proto_msgTypes[6]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use HeartbeatAck.ProtoReflect.Descriptor instead.
+func (*HeartbeatAck) Descriptor() ([]byte, []int) {
+	return file_logic_game_proto_rawDescGZIP(), []int{6}
+}
+
+func (x *HeartbeatAck) GetServerTime() int64 {
+	if x != nil {
+		return x.ServerTime
+	}
+	return 0
+}
+
+func (x *HeartbeatAck) GetRttMs() int64 {
+	if x != nil {
+		return x.RttMs
+	}
+	return 0
+}
+
 var File_logic_game_proto protoreflect.FileDescriptor
 
 var file_logic_game_proto_rawDesc = []byte{
@@ -483,10 +585,17 @@ var file_logic_game_proto_rawDesc = []byte{
 	0x45, 0x6e, 0x64, 0x54, 0x69, 0x6d, 0x65, 0x18, 0x02, 0x20, 0x01, 0x28, 0x03, 0x52, 0x0a, 0x62,
 	0x61, 0x6e, 0x45, 0x6e, 0x64, 0x54, 0x69, 0x6d, 0x65, 0x12, 0x1c, 0x0a, 0x09, 0x62, 0x61, 0x6e,
 	0x52, 0x65, 0x61, 0x73, 0x6f, 0x6e, 0x18, 0x03, 0x20, 0x01, 0x28, 0x09, 0x52, 0x09, 0x62, 0x61,
-	0x6e, 0x52, 0x65, 0x61, 0x73, 0x6f, 0x6e, 0x42, 0x25, 0x5a, 0x23, 0x67, 0x69, 0x74, 0x68, 0x75,
-	0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x73, 0x74, 0x72, 0x65, 0x61, 0x73, 0x75, 0x72, 0x65, 0x2f,
-	0x70, 0x72, 0x6f, 0x74, 0x6f, 0x63, 0x6f, 0x6c, 0x2f, 0x6c, 0x6f, 0x67, 0x69, 0x63, 0x62, 0x06,
-	0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x6e, 0x52, 0x65, 0x61, 0x73, 0x6f, 0x6e, 0x22, 0x2f, 0x0a, 0x0c, 0x48, 0x65, 0x61, 0x72, 0x74,
+	0x62, 0x65, 0x61, 0x74, 0x52, 0x65, 0x71, 0x12, 0x1f, 0x0a, 0x0b, 0x63, 0x6c, 0x69, 0x65, 0x6e,
+	0x74, 0x5f, 0x74, 0x69, 0x6d, 0x65, 0x18, 0x01, 0x20, 0x01, 0x28, 0x03, 0x52, 0x0a, 0x63, 0x6c,
+	0x69, 0x65, 0x6e, 0x74, 0x54, 0x69, 0x6d, 0x65, 0x22, 0x46, 0x0a, 0x0c, 0x48, 0x65, 0x61, 0x72,
+	0x74, 0x62, 0x65, 0x61, 0x74, 0x41, 0x63, 0x6b, 0x12, 0x1f, 0x0a, 0x0b, 0x73, 0x65, 0x72, 0x76,
+	0x65, 0x72, 0x5f, 0x74, 0x69, 0x6d, 0x65, 0x18, 0x01, 0x20, 0x01, 0x28, 0x03, 0x52, 0x0a, 0x73,
+	0x65, 0x72, 0x76, 0x65, 0x72, 0x54, 0x69, 0x6d, 0x65, 0x12, 0x15, 0x0a, 0x06, 0x72, 0x74, 0x74,
+	0x5f, 0x6d, 0x73, 0x18, 0x02, 0x20, 0x01, 0x28, 0x03, 0x52, 0x05, 0x72, 0x74, 0x74, 0x4d, 0x73,
+	0x42, 0x25, 0x5a, 0x23, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x73,
+	0x74, 0x72, 0x65, 0x61, 0x73, 0x75, 0x72, 0x65, 0x2f, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x63, 0x6f,
+	0x6c, 0x2f, 0x6c, 0x6f, 0x67, 0x69, 0x63, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -501,17 +610,19 @@ func file_logic_game_proto_rawDescGZIP() []byte {
 	return file_logic_game_proto_rawDescData
 }
 
-var file_logic_game_proto_msgTypes = make([]protoimpl.MessageInfo, 5)
+var file_logic_game_proto_msgTypes = make([]protoimpl.MessageInfo, 7)
 var file_logic_game_proto_goTypes = []interface{}{
 	(*LoginReq)(nil),              // 0: logic.LoginReq
 	(*LoginAck)(nil),              // 1: logic.LoginAck
 	(*LogoutReq)(nil),             // 2: logic.LogoutReq
 	(*LogoutAck)(nil),             // 3: logic.LogoutAck
 	(*BanInfo)(nil),               // 4: logic.BanInfo
-	(*commonstruct.UserInfo)(nil), // 5: commonstruct.UserInfo
+	(*HeartbeatReq)(nil),          // 5: logic.HeartbeatReq
+	(*HeartbeatAck)(nil),          // 6: logic.HeartbeatAck
+	(*commonstruct.UserInfo)(nil), // 7: commonstruct.UserInfo
 }
 var file_logic_game_proto_depIdxs = []int32{
-	5, // 0: logic.LoginAck.user:type_name -> commonstruct.UserInfo
+	7, // 0: logic.LoginAck.user:type_name -> commonstruct.UserInfo
 	4, // 1: logic.LoginAck.banInfo:type_name -> logic.BanInfo
 	2, // [2:2] is the sub-list for method output_type
 	2, // [2:2] is the sub-list for method input_type
@@ -586,6 +697,30 @@ func file_logic_game_proto_init() {
 				return nil
 			}
 		}
+		file_logic_game_proto_msgTypes[5].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*HeartbeatReq); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_logic_game_proto_msgTypes[6].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*HeartbeatAck); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
@@ -593,7 +728,7 @@ func file_logic_game_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_logic_game_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   5,
+			NumMessages:   7,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
